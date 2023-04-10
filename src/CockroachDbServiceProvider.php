f78b8c18f146af2fda0f4720a02368ea5629a481
Stupid Dev <vuthaihoc.ttud@gmail.com>
2023-04-10 16:41:30 +0700

    Support multi read/write hosts

--- src/CockroachDbServiceProvider.php
+++ src/CockroachDbServiceProvider.php
@@ -4,6 +4,7 @@ namespace YlsIdeas\CockroachDb;
 
 use Illuminate\Database\Connection;
 use Illuminate\Database\DatabaseManager;
+use Illuminate\Support\Arr;
 use Illuminate\Support\ConfigurationUrlParser;
 use Illuminate\Support\ServiceProvider;
 
@@ -11,14 +12,29 @@ class CockroachDbServiceProvider extends ServiceProvider
 {
     public function register()
     {
+
         $this->app->extend(DatabaseManager::class, function (DatabaseManager $manager) {
             ConfigurationUrlParser::addDriverAlias('cockroachdb', 'crdb');
 
-            Connection::resolverFor('crdb', function ($connection, $database, $prefix, $config) {
+            Connection::resolverFor('crdb', function ($connection, $database, $prefix, $config) use ($manager) {
                 $connector = new CockroachDbConnector();
-                $connection = $connector->connect($config);
 
-                return new CockroachDbConnection($connection, $database, $prefix, $config);
+                $hosts = Arr::wrap($config['host']);
+                foreach (Arr::shuffle($hosts) as $host) {
+                    $config['host'] = $host;
+                    try {
+                        $pdo = $connector->connect($config);
+                        break;
+                    } catch (PDOException $e) {
+                        continue;
+                    }
+                }
+
+                return new CockroachDbConnection($pdo, $database, $prefix, $config);
+            });
+
+            $this->app->bind('db.connector.crdb', function(){
+                return new CockroachDbConnector();
             });
 
             return $manager;
